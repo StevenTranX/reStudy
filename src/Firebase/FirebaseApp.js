40b0e19e1f5a6ff9b40b795f4e0dcf41153f4072
Steven Tran <trantrongtinnna3k55@gmail.com>
2022-12-19 11:38:01 +0700

    firebase method

--- src/Firebase/FirebaseApp.js
+++ src/Firebase/FirebaseApp.js
@@ -1,60 +1,104 @@
-import { collection, getDocs, addDoc } from "firebase/firestore";
-import postcss from "postcss";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs, addDoc , deleteDoc, doc } from "firebase/firestore";
+// doc ở đây là 1 document cụ thể doc( firestore , path , id  )
 import { db } from "./firebase-config";
-const colRef = collection(db, "posts");
+
 const FirebaseApp = () => {
+  const colRef = collection(db, "posts");
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [postId, setPostId] = useState()
   // colRef - collection ref
-
   useEffect(() => {
-    // * 1. Get collection data (posts)
-
+    // 1. Get collection from data (posts) bằng cách dùng hàm getDocs
     getDocs(colRef)
       .then((snapshot) => {
+        // 2 Sau đó .then và lấy item snapshow ra
+        console.log(snapshot);
         let posts = [];
         snapshot.docs.forEach((doc) => {
+          // Duyệt snapshot.docs - chữ mờ rồi dùng hàm data() để lấy các trường mình generate ở store ra
+
+          // ?? Khi ta console.log(snapshot) thì ta không hề thấy data ở đâu cả những sau khi dùng data() thì ta truy xuất được ??
+          // * đây là cách hoạt động của firebase - data nằm ở snapshot.document.data.value.mapValue.fields
+          // * ta chỉ cần dùng hàm data() là lấy được
+
           posts.push({
             id: doc.id,
             ...doc.data(),
           });
+          // Sau đó push data vào mảng rỗng, lấy riêng id và clone data
         });
         console.log(posts);
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((error) => {
+        console.log(error);
+        // nhớ catch để bắt lỗi.
       });
   }, []);
   const handleAddPost = (e) => {
     e.preventDefault();
     addDoc(colRef, {
-      title: "",
-      author: "",
+      // addDoc là 1 promise
+      title,
+      author
+      // * Ở đây thay vì viết title : title từ useState thì ta viết như trên trong es6 
     })
-      .then(() => console.log("success"))
-      .catch((error) => {
-        console.log(error);
+      .then(() => {
+        console.log("success");
+        // reset form
+      })
+      .catch((err) => {
+        console.log(err);
       });
   };
+  const handleRemovePost = async (e) => {
+    e.preventDefault();
+    const colRefDelete = doc(db, 'posts', postId)
+    await deleteDoc(colRefDelete)
+    console.log("Delete Success");
+  }
   return (
-    <div className="p-10">
-      <div className="w-full mx-auto max-w-[500px] bg-white shadow-lg p-5">
+    <div className='p-10'>
+      <div className='w-full mx-auto max-w-[500px] bg-white shadow-lg p-5 mb-10'>
         <form onSubmit={handleAddPost}>
           <input
-            type="text"
-            className="p-3 rounded border border-gray-300 w-full mb-5 outline-none focus:border-blue-500"
-            placeholder="Enter your title"
-            name="title"
+            type='text'
+            className='p-3 rounded border border-gray-300 w-full mb-5 outline-none focus:border-blue-500'
+            placeholder='Enter your title'
+            name='title'
+            onChange={e => {setTitle(e.target.value)}}
           />
           <input
-            type="text"
-            className="p-3 rounded border border-gray-300 w-full mb-5 outline-none focus:border-blue-500"
-            placeholder="Enter your author"
-            name="author"
+            type='text'
+            className='p-3 rounded border border-gray-300 w-full mb-5 outline-none focus:border-blue-500'
+            placeholder='Enter your author'
+            name='author'
+            onChange={e => {setAuthor(e.target.value)}}
           />
           <button
-            type="submit"
-            className="p-3 bg-blue-500 text-white text-sm font-medium "
-          ></button>
+            type='submit'
+            className='p-3 bg-blue-500 text-white text-sm font-medium '
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className='w-full mx-auto max-w-[500px] bg-white shadow-lg p-5 mb-10'>
+        <form onSubmit={handleRemovePost}>
+          <input
+            type='text'
+            className='p-3 rounded border border-gray-300 w-full mb-5 outline-none focus:border-blue-500'
+            placeholder='Enter your id'
+            name='postId'
+            onChange={e => {setPostId(e.target.value)}}
+          />
+          <button
+            type='submit'
+            className='p-3 bg-red-500 text-white text-sm font-medium '
+          >
+            Remove Post
+          </button>
         </form>
       </div>
     </div>
--- src/Firebase/firebase-config.js
+++ src/Firebase/firebase-config.js
@@ -1,18 +1,17 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
 const firebaseConfig = {
-  apiKey: 'AIzaSyDzQosGehZYY5CbBn97tfzlVtalxMLsSSE',
-  authDomain: 'learn-firebase-99fc5.firebaseapp.com',
-  projectId: 'learn-firebase-99fc5',
-  storageBucket: 'learn-firebase-99fc5.appspot.com',
-  messagingSenderId: '309093659197',
-  appId: '1:309093659197:web:358094c52c3aaff5dcb054',
-  measurementId: 'G-X2JC090TJS',
+  apiKey: "AIzaSyDzQosGehZYY5CbBn97tfzlVtalxMLsSSE",
+  authDomain: "learn-firebase-99fc5.firebaseapp.com",
+  projectId: "learn-firebase-99fc5",
+  storageBucket: "learn-firebase-99fc5.appspot.com",
+  messagingSenderId: "309093659197",
+  appId: "1:309093659197:web:358094c52c3aaff5dcb054",
+  measurementId: "G-X2JC090TJS"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//Init services
-export const db = getFirestore(app);
+// Init services 
+export const db  = getFirestore(app)
